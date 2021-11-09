ef0a0859c855064f941ead2c0368a8e32010f47e
Ryan <snorekr@gmail.com>
2021-11-08 19:22:28 -0500

    show

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -23,28 +23,40 @@ const testShow = {
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={"none"} />);
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show show={null}/>);
+    const loading = screen.queryByTestId("loading-container");
+    expect(loading).toBeInTheDocument();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={"none"} />);
+    const seasons = screen.queryAllByTestId("season-option");
+    expect(seasons.length).toBe(2);
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const fakeHandleSelect = jest.fn();
+
+    render(<Show show={testShow} selectedSeason={"none"} handleSelect={fakeHandleSelect} />);
+    
+    const select = screen.getByRole("combobox");
+
+    // expect(fakeHandleSelect).toHaveBeenCalled();
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={testShow} selectedSeason={"none"} />);
+
+    // Episode does not render
+
+    rerender(<Show show={testShow} selectedSeason={testShow.seasons[0]} />)
+    // Does render
+
 });
-
-//Tasks:
-//1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
-
-//2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
-
-//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
-
-//4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
 
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
 
